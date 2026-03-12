9ddcb7f4c9c20e153b667b06d97c20de942eb481
Winner <obebe.elizabeth@gmail.comgit config --global user.email obebe.elizabeth@gmail.com>
2026-03-12 10:17:57 +0100

    Fix live superuser creation

--- quiz_arena/build.sh
+++ quiz_arena/build.sh
@@ -8,10 +8,10 @@ echo "=== RUNNING MIGRATIONS ==="
 python manage.py migrate
 
 echo "=== LOADING QUESTIONS FIXTURE ==="
-python manage.py loaddata questions.json --verbosity 2
+python manage.py loaddata questions.json --verbosity 2 || true
 
 echo "=== CREATING SUPERUSER IF NEEDED ==="
-python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); username='Wums'; email='obebe.elizabeth@gmail.com'; password='WumsAdmin123!';  User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); username='wums'; email='obebe.elizabeth@gmail.com'; password='WumsAdmin123!'; User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
 
 echo "=== COLLECTING STATIC FILES ==="
 python manage.py collectstatic --noinput
