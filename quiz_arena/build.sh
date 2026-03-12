acd986aed2ed76882f3a1ae394fa960c98ef798d
Winner <obebe.elizabeth@gmail.comgit config --global user.email obebe.elizabeth@gmail.com>
2026-03-12 09:19:55 +0100

    Create admin user on deploy

--- quiz_arena/build.sh
+++ quiz_arena/build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+set -e
 
+echo "=== INSTALLING REQUIREMENTS ==="
 pip install -r requirements.txt
 
+echo "=== RUNNING MIGRATIONS ==="
 python manage.py migrate
 
-python manage.py loaddata questions.json
+echo "=== LOADING QUESTIONS FIXTURE ==="
+python manage.py loaddata questions.json --verbosity 2
 
+echo "=== CREATING SUPERUSER IF NEEDED ==="
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); username='Wums'; email='obebe.elizabeth@gmail.com'; password='WumsAdmin123!';  User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
+
+echo "=== COLLECTING STATIC FILES ==="
 python manage.py collectstatic --noinput
