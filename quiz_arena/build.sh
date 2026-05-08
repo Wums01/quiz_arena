5ace1e65d0d2f9cdf7216ca72fac152f53eff7f5
Winner <obebe.elizabeth@gmail.comgit config --global user.email obebe.elizabeth@gmail.com>
2026-05-08 13:51:33 +0100

    trigger fresh render deploy

--- quiz_arena/build.sh
+++ quiz_arena/build.sh
@@ -13,5 +13,5 @@ python manage.py loaddata questions.json --verbosity 2 || true
 echo "=== CREATING SUPERUSER IF NEEDED ==="
 python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); username='wums'; email='obebe.elizabeth@gmail.com'; password='WumsAdmin123!'; User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
 
-echo "=== COLLECTING STATIC FILES ==="
+echo "=== COLLECTING STATIC FILES FOR QUIZ ARENA ==="
 python manage.py collectstatic --noinput
